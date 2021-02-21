7672baef9be8b7a0077ee2d75e43df70c906f389
Quinn T <Quinn.GitHub@gmail.com>
2021-02-21 17:18:26 -0600

    rainbow support

--- color.js
+++ color.js
@@ -1,18 +1,22 @@
 const config = require("./config");
-
-// script
 const ircLib = require("dank-twitch-irc");
+const Color = require('color');
+var rainbowColor = Color('hsl(0, 60%, 50%)');
+rainbowColor = rainbowColor.rotate(config.rainbowStartHue);
 const bot_config = {
     username: config.username, // justinfan12345 by default - For anonymous chat connection
     password: config.oauth, // undefined by default (no password)
 }
-const client = new ircLib.ChatClient(bot_config);
 let client_ready = false;
-function randInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+const client = new ircLib.ChatClient(bot_config);
+
+// Generates a random number from 0-limit (number will never = limit, e.g. `randInt(1)` always gives 0) 
+function randInt(limit) {
+    return Math.floor(Math.random() * Math.floor(limit));
 }
 
 client.on("ready", () => {
+    // when the client is ready, say that we've connected, tell the program its connected, and send information about configiration.
     console.log(`${new Date().toLocaleTimeString()} | INFO - Connected to chat and ready to send colors.`)
     client_ready = true;
     showInfo()
@@ -30,8 +34,14 @@ function randomHex() {
     return out;
 }
 
+function rainbowHex() {
+    rainbowColor = rainbowColor.rotate(config.rainbowSpeed);  // increaces the hue by the speed, think of it like rotating it on the color wheel.
 
-function primeColor() {
+    return rainbowColor.hex()
+}
+
+
+function nonPrimeColor() {
     let out = "";
     const primeColors = ["Blue", "BlueViolet", "CadetBlue", "Chocolate", "Coral", "DodgerBlue", "Firebrick", "GoldenRod", "Green", "HotPink", "OrangeRed", "Red", "SeaGreen", "SpringGreen", "YellowGreen"];
     
@@ -43,8 +53,15 @@ function primeColor() {
 
 function showInfo() {
     let primeMessage = "Prime colors are off, if you would like to have more colors, turn it on. (Requires Prime/Turbo)"
-    if(config.hasPrime) {
+    if(config.usePrimeColors) {
         primeMessage = "Prime colors are on. If its not doing anything, try turning them off."
+    }
+    if(config.useRainbow) {
+        let rainbowMessage = `Rainbow is on. Speed: ${config.rainbowSpeed}`;
+        if (!config.usePrimeColors) {
+            rainbowMessage = "Rainbow is on, but prime colors are off. Using random defalt color."
+        }
+        console.log(`${new Date().toLocaleTimeString()} | INFO - ${rainbowMessage}`)
     }
     console.log(`${new Date().toLocaleTimeString()} | INFO - All commands are only sent to YOUR chat (#${config.username})`)
     console.log(`${new Date().toLocaleTimeString()} | INFO - ${primeMessage}`)
@@ -52,10 +69,14 @@ function showInfo() {
 
 setInterval(() => {
     let color = "";
-    if(config.hasPrime) {
-        color = randomHex()
+    if(config.usePrimeColors) {
+        if ( config.useRainbow ) {
+            color = rainbowHex();
+        } else {
+            color = randomHex()
+        }
     } else {
-        color = primeColor()
+        color = nonPrimeColor()
     }
     if (client_ready) {
         console.log(`${new Date().toLocaleTimeString()} | ${color}`)
--- config.example.js
+++ config.example.js
@@ -5,16 +5,35 @@
 // and then the changes you make there will take effect.
 
 module.exports = {
-    // change to true to have ANY color (needs twitch prime or turbo)
-    hasPrime: false,
+
     // this is your username, it must be in lowercase
     username: "example_user",
+
     // this is an oauth token to login to chat, you can get one here: https://twitchapps.com/tmi/ (you dont need the "oauth:" part)
     oauth: "xxxxxxxxxxxxxxxxxxxxxx",
+
     // this is how often the script will send a new color, you have to keep this in twitch ratelimits. 
     // See https://dev.twitch.tv/docs/irc/guide#command--message-limits for more information
-    seconds: 15
-    // just dont set it super low basicly, 5 seconds is as low as I would go.
+    // just dont set it super low basicly, 10 seconds is as low as I would go.
     // just keep in mind that if you plan on spamming, you can only spam up to 20 messages per 30 seconds without mod/vip
     // and this will be part of that. (if you go over you wont be able to send any messages for about 30 mins)
+    seconds: 15,
+
+    // change to true to have ANY color (needs twitch prime or turbo)
+    usePrimeColors: false,
+
+    // This will only work if hasPrime is true
+    // it will go through all colors in the rainbow, in order.
+    useRainbow: false,
+
+    // this is by how much the hue will change every color
+    // negative numbers will go through the rainbow backwords.
+    // at this speed, it will be very slow, almost unnoticable. 
+    // but over a few mins it will look cool.
+    rainbowSpeed: 1,
+
+    // This is for if you want to start the rainbow in the middle or something. 
+    // it will increace the hue by this ammount before the first color 
+    rainbowStartHue: 0,
+
 }
